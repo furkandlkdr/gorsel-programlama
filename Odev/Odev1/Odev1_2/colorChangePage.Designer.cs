bfb62b6b59db0206db84a432082f2fc6a4a24019
furkandlkdr <115067547+furkandlkdr@users.noreply.github.com>
2024-03-05 23:56:16 +0300

    Added colorChangePage and finished second hw.

---
+++ Odev/Odev1/Odev1_2/colorChangePage.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Odev1_2
+{
+    partial class appForm
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorCombo = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // colorCombo
+            // 
+            this.colorCombo.FormattingEnabled = true;
+            this.colorCombo.Location = new System.Drawing.Point(92, 27);
+            this.colorCombo.Name = "colorCombo";
+            this.colorCombo.Size = new System.Drawing.Size(121, 21);
+            this.colorCombo.TabIndex = 0;
+            this.colorCombo.Text = "Form Rengi";
+            this.colorCombo.SelectedIndexChanged += new System.EventHandler(this.colorCombo_SelectedIndexChanged);
+            // 
+            // appForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 172);
+            this.Controls.Add(this.colorCombo);
+            this.Name = "appForm";
+            this.Text = "Renk Değiştirme";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox colorCombo;
+    }
+}
+
---
+++ Odev/Odev1/Odev1_2/colorChangePage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Odev1_2
+{
+    public partial class appForm : Form
+    {
+        public string[] colors = { "Red", "Green", "Blue", "Yellow", "Black", "White", "Purple", "Orange", "Pink", "Brown" };
+        string choosenColor = "White";
+        public appForm()
+        {
+            InitializeComponent();
+            colorCombo.DataSource = colors;
+        }
+
+        private void colorCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                choosenColor = colorCombo.SelectedItem.ToString();
+                this.BackColor = System.Drawing.Color.FromName(choosenColor);
+            }
+            catch { }
+        }
+    }
+}
