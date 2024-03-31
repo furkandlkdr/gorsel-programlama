66481d03287d82aee183825ba9276a2ec3913914
furkandlkdr <221030910016@kayseri.edu.tr>
2024-03-31 16:03:13 +0300

    added hw4

---
+++ Odev/Odev4/Odev4/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Odev4
+{
+    partial class PasswordRandomizer
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            passwordLabel = new Label();
+            button0 = new Button();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            SuspendLayout();
+            // 
+            // passwordLabel
+            // 
+            passwordLabel.BackColor = Color.Cornsilk;
+            passwordLabel.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            passwordLabel.Location = new Point(77, 9);
+            passwordLabel.Name = "passwordLabel";
+            passwordLabel.Size = new Size(108, 23);
+            passwordLabel.TabIndex = 0;
+            passwordLabel.Text = "_ _ _ _";
+            passwordLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // button0
+            // 
+            button0.BackColor = Color.Cornsilk;
+            button0.FlatStyle = FlatStyle.Flat;
+            button0.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            button0.Location = new Point(77, 35);
+            button0.Name = "button0";
+            button0.Size = new Size(32, 31);
+            button0.TabIndex = 1;
+            button0.Text = "0";
+            button0.UseVisualStyleBackColor = false;
+            button0.MouseClick += number_click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Cornsilk;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            button1.Location = new Point(115, 35);
+            button1.Name = "button1";
+            button1.Size = new Size(32, 31);
+            button1.TabIndex = 2;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = false;
+            button1.MouseClick += number_click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Cornsilk;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            button2.Location = new Point(153, 35);
+            button2.Name = "button2";
+            button2.Size = new Size(32, 31);
+            button2.TabIndex = 3;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = false;
+            button2.MouseClick += number_click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Cornsilk;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            button3.Location = new Point(77, 72);
+            button3.Name = "button3";
+            button3.Size = new Size(32, 31);
+            button3.TabIndex = 4;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = false;
+            button3.MouseClick += number_click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Cornsilk;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            button4.Location = new Point(115, 72);
+            button4.Name = "button4";
+            button4.Size = new Size(32, 31);
+            button4.TabIndex = 5;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = false;
+            button4.MouseClick += number_click;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.Cornsilk;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            button5.Location = new Point(153, 72);
+            button5.Name = "button5";
+            button5.Size = new Size(32, 31);
+            button5.TabIndex = 6;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = false;
+            button5.MouseClick += number_click;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.Cornsilk;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            button6.Location = new Point(77, 109);
+            button6.Name = "button6";
+            button6.Size = new Size(32, 31);
+            button6.TabIndex = 7;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = false;
+            button6.MouseClick += number_click;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.Cornsilk;
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            button7.Location = new Point(115, 109);
+            button7.Name = "button7";
+            button7.Size = new Size(32, 31);
+            button7.TabIndex = 8;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = false;
+            button7.MouseClick += number_click;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.Cornsilk;
+            button8.FlatStyle = FlatStyle.Flat;
+            button8.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            button8.Location = new Point(153, 109);
+            button8.Name = "button8";
+            button8.Size = new Size(32, 31);
+            button8.TabIndex = 9;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = false;
+            button8.MouseClick += number_click;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.Cornsilk;
+            button9.FlatStyle = FlatStyle.Flat;
+            button9.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            button9.Location = new Point(115, 146);
+            button9.Name = "button9";
+            button9.Size = new Size(32, 31);
+            button9.TabIndex = 10;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = false;
+            button9.MouseClick += number_click;
+            // 
+            // PasswordRandomizer
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.CornflowerBlue;
+            ClientSize = new Size(261, 196);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(button0);
+            Controls.Add(passwordLabel);
+            Name = "PasswordRandomizer";
+            Text = "Password Randomizer";
+            Load += PasswordRandomizer_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label passwordLabel;
+        private Button button0;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+    }
+}
---
+++ Odev/Odev4/Odev4/Form1.cs
@@ -0,0 +1,56 @@
+﻿namespace Odev4
+{
+    public partial class PasswordRandomizer : Form
+    {
+        int[][] positions = new int[10][];
+        string password = "1 2 3 4";
+        public PasswordRandomizer()
+        {
+            InitializeComponent();
+        }
+
+        private void PasswordRandomizer_Load(object sender, EventArgs e)
+        {
+            int i = 0;
+            foreach (Control control in Controls)
+            {
+                if (control is Button b)
+                {
+                    positions[i] = new int[2];
+                    positions[i][0] = b.Location.X;
+                    positions[i][1] = b.Location.Y;
+                    i++;
+                }
+            }
+        }
+
+        private void number_click(object sender, MouseEventArgs e){
+            if (e.Button == MouseButtons.Left){
+                for (int i = 0; i < 10; i++) {
+                    int randomIndex = new Random().Next(0, 10);
+                    Button button1 = (Button)Controls.Find("button" + randomIndex, true)[0];
+                    Button button2 = (Button)Controls.Find("button" + i, true)[0];
+
+                    button1.Location = new Point(positions[i][0], positions[i][1]);
+                    button2.Location = new Point(positions[randomIndex][0], positions[randomIndex][1]);
+                    positions[randomIndex] = [button1.Location.X, button1.Location.Y];
+                    positions[i] = [button2.Location.X, button2.Location.Y];
+                }
+
+                Button b = (Button)sender;
+                int foundedIndex = passwordLabel.Text.IndexOf('_');
+                if (foundedIndex != -1)
+                    passwordLabel.Text = passwordLabel.Text.Remove(foundedIndex, 1).Insert(foundedIndex, b.Text);
+                if (passwordLabel.Text.IndexOf('_') == -1){
+                    if (passwordLabel.Text == password){
+                        MessageBox.Show("Giriş başarılı!");
+                        Environment.Exit(0);
+                    } else{
+                        MessageBox.Show("Şifre hatalı, tekrar deneyin!");
+                    }
+                    passwordLabel.Text = "_ _ _ _";
+                }
+            }
+        }
+    }
+}
