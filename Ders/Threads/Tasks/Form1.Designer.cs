95dbc0e2a428be84b031d25e9e378efb1a6066e2
Furkan Dulkadir <221030910016@kayseri.edu.tr>
2024-05-22 15:00:49 +0300

    Added Task example

---
+++ Ders/Threads/Tasks/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Tasks {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_Basla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // btn_Basla
+            // 
+            this.btn_Basla.Location = new System.Drawing.Point(53, 99);
+            this.btn_Basla.Name = "btn_Basla";
+            this.btn_Basla.Size = new System.Drawing.Size(75, 23);
+            this.btn_Basla.TabIndex = 2;
+            this.btn_Basla.Text = "Başlat";
+            this.btn_Basla.UseVisualStyleBackColor = true;
+            this.btn_Basla.Click += new System.EventHandler(this.btn_Basla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(192, 134);
+            this.Controls.Add(this.btn_Basla);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_Basla;
+    }
+}
+
---
+++ Ders/Threads/Tasks/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Tasks {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btn_Basla_Click(object sender, EventArgs e) {
+            CheckForIllegalCrossThreadCalls = false;
+            Task task1 = Task.Factory.StartNew(() => {
+                for (int i = 0; i < 30; i++) {
+                    Thread.Sleep(500);
+                    label1.Text = i.ToString();
+                }
+            });
+            Task task2 = Task.Factory.StartNew(() => {
+                for (int i = 0; i < 30; i++) {
+                    Thread.Sleep(500);
+                    label2.Text = i.ToString();
+                }
+            });
+        }
+    }
+}
