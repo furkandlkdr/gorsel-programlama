744d9b4cd84032e5b483140809afecff4974640b
Furkan Dulkadir <221030910016@kayseri.edu.tr>
2024-05-22 13:50:16 +0300

    Added thread's first examp.

---
+++ Ders/Threads/Threads/Thread_Mantik.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Threads
+{
+    public partial class Thread_Mantik : Form
+    {
+        public Thread_Mantik()
+        {
+            InitializeComponent();
+        }
+        private void Dongu()
+        {
+            decimal i = default(decimal);
+            for (i= 0; i <= 1000000000; i++)
+            {
+            }
+            MessageBox.Show("Döngü Bitti");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dongu();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Başka bir kanal üzerinde bu işlemi gerçekleştirdiği için diğer işlemleri
+            // engellemez. Mesaj yazdırabiliriz.
+            Thread kanal1 = new Thread(Dongu);
+            kanal1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Merhaba");
+        }
+    }
+}
